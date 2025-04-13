768e7a337eaa8c0645b437a171aa187fb79d9034
jreleaserbot <jreleaser@kordamp.org>
2025-04-13 22:44:55 +0200

    contracteer 2.0.0

--- Formula/contracteer.rb
+++ Formula/contracteer.rb
@@ -1,11 +1,11 @@
-# Generated with JReleaser 1.17.0 at 2025-04-13T21:58:02.028686+02:00
+# Generated with JReleaser 1.17.0 at 2025-04-13T22:43:05.809102+02:00
 
 class Contracteer < Formula
   desc "The musketeer of your API contracts."
   homepage "https://github.com/camory/contracteer"
   url "https://github.com/camory/contracteer/releases/download/2.0.0/contracteer-2.0.0-osx-aarch_64.zip"
   version "2.0.0"
-  sha256 "bc338dc659d29d1ac24be271f1d432fd23b338d100ef1eb63ddb19635b2015b9"
+  sha256 "7637167f57effed00343f397fef13507536977e39d490b71862200df8232ff5e"
   license "GPL-3.0-only"
 
   livecheck do
